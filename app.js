63e2a0b3b4a0aec6237a5441d2fbac166017be3d
jucetina <juliethcetina@gmail.com>
2021-05-16 20:49:11 -0500

    Se agregan los estilos de los botones del slider

--- app.js
+++ app.js
@@ -6,6 +6,26 @@ const email = new MDCTextField(document.querySelector('.email'));
 const password = new MDCTextField(document.querySelector('.password'));
 const ripple = new MDCRipple(document.querySelector('.social-button'));
 
+/*** Funcionalidad para agregar colores en los botones del slider ***/
+
+//Al cargar la página por primera vez, le cambia el color al primer botón del slider
+let firstButton = document.querySelector('.slider__menu-button');
+firstButton.style.backgroundColor = '#f9b76b'; 
+
+//Se obtienen todos los botones del slider en un NodeList
+let buttons = document.querySelectorAll('.slider__menu-button');
+
+//El resultado se convierte a array para poder utilizar el método .map()
+let buttonsArray = [...buttons];
+
+//A cada boton del slider se le agrega un escuchador de evento de click
+let buttonClicked = buttonsArray.map(button => button.addEventListener('click', e => {
+    //Al dar click, todos los botones se vuelven blancos
+    buttonsArray.map(button => button.style.backgroundColor = '#fff');
+    //El boton clickeado se vuelve de color #f9b76b
+    e.target.style.backgroundColor = '#f9b76b';
+}));
+
 /*** Funcionalidades para el campo de contraseña ***/
 
 //Cambiar icono del campo de contraseña si está vacío o no
