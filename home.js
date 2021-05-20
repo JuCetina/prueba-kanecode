ea9ee1e602cd2a5fa9e42a4e4597b5eb63500202
jucetina <juliethcetina@gmail.com>
2021-05-20 03:09:57 -0500

    Se agrega la animación del icono del menu del usuario al darle click  y se inicia la estructura en html y los estilos de los submenu de la barra superior

--- home.js
+++ home.js
@@ -16,3 +16,39 @@ const showHideMenu = () => {
 }
 
 menuButton.addEventListener('click', showHideMenu);
+
+/** Submenús **/
+
+/** Submnú Help **/
+
+let helpButton = document.querySelector('.bar__user-icon-help');
+let helpSubmenu = document.querySelector('.bar__help-submenu-container');
+
+const showHideHelpSubmenu = () => {
+    helpSubmenu.classList.toggle('is-active');
+}
+
+helpButton.addEventListener('click', showHideHelpSubmenu);
+
+/** Submenú Notifications **/
+
+let notificationsButton = document.querySelector('.bar__user-icon-notifications');
+let notificationsSubmenu = document.querySelector('.bar__notifications-submenu');
+
+const showHideNotificationsSubmenu = () => {
+    notificationsSubmenu.classList.toggle('is-active');
+}
+
+notificationsButton.addEventListener('click', showHideNotificationsSubmenu);
+
+/** Submenú User **/
+
+let userButton = document.querySelector('.bar__user-icon--arrow');
+let userSubmenu = document.querySelector('.bar__user-submenu');
+
+const showHideUserSubmenu = () => {
+    userButton.classList.toggle('is-active');
+    userSubmenu.classList.toggle('is-active');
+}
+
+userButton.addEventListener('click', showHideUserSubmenu);
